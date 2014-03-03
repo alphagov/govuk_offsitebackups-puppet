d67adeed9ee54d94621789284a2ce411621c3b5e
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2014-03-03 15:35:15 +0000

    Amends firewall.pp to fix lint test
    Lint test was failing due to lack of hash in comment on line 1. Added hash. Lint passes.

--- modules/base/manifests/firewall.pp
+++ modules/base/manifests/firewall.pp
@@ -1,9 +1,9 @@
-== Class: base firewall
+# == Class: base firewall
 #
 # Configures ufw, allowing port 22 for SSH
 #
 class base::firewall {
-  
+ 
   ufw::allow { 'allow-ssh':
     port => '22',
     ip   => 'any'
