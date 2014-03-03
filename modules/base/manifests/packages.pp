8d84b86987d80f763c1008cf6ad360c3886557db
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2014-03-03 14:33:21 +0000

    Add base packages
    This file lists an array of packages to install to the destination server. It also ensures that the file passes rake lint tests by fixing soft-tab spacing within the array.

--- modules/base/manifests/packages.pp
+++ modules/base/manifests/packages.pp
@@ -1,10 +1,14 @@
+# This file controls the packages installed on destination machines.
+# It can be easily updated by adding new package names to the array.
+
 class base::packages {
 
   package {
     [
-     'ack-grep',
-     'bzip2',
-     'tar',
+      'ack-grep',
+      'bzip2',
+      'tar',
     ]
+    ensure => installed;
   }
 }
