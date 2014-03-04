75acec6df1e26bd9b859adea676706adb406a7c8
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2014-03-04 12:42:21 +0000

    Add mounts manifest
    This commit adds a mounts.pp manifest, which configures and mounts LVM disks.
    It always checks for the existence of a `backup-data` dir at /dev/sdb, which it creates if required.

---
+++ modules/base/manifests/mounts.pp
@@ -0,0 +1,31 @@
+# == Class: base::mounts
+# 
+# Assuming the VM is not a dev VM, then:
+#
+# - configure LVM disks
+# - mount LVM disks
+#
+# Always ensure `backup_data` directory exists
+
+class base::mounts {
+
+    file { '/srv/backup_data':
+        ensure =>   directory,
+        owner  =>   'govuk-backup',
+    }
+
+    lvm::volume { 'data':
+        ensure  =>  present,
+        pv      =>  '/dev/sdb',
+        vg      =>  'backup',
+        fstype  =>  'ext4',
+    }
+
+    ext4mount { '/srv/backup-data':
+        mountoptions  =>  'defaults',
+        disk          =>  '/dev/mapper/backup-data',
+        before        =>  File['/srv/backup-data'],
+        require       =>  Lvm::Volume['data'],
+    }
+
+}
