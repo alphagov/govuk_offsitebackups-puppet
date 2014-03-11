52c358c8bc69e5af84e44ae105d67d98e2b10a50
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2014-03-11 15:39:05 +0000

    Add shell script to scan /srv/backup-data
    This commit adds a shell script to scan the contents of /srv/backup-data, log
    output to a specified .log file, and move infected data to /srv/infected

---
+++ modules/base/files/usr/local/sbin/scan-backup-data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# A script to serve /srv/backup-data
+
+echo "Scan of /srv/backup-data started at `date --rfc-3339=s`" >> /var/log/clamav/clamdscan.log
+/usr/bin/clamdscan.log --quiet -l /var/log/clamav/clamdscan.log --move /srv/infected /srv/backup-data
