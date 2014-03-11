01e470b52b018b563ded1eb57881593573ea094e
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2014-03-11 15:42:36 +0000

    Add clamav.pp manifest to control ClamAV scanning
    This commit adds a manifest (clamav.pp) to Puppet in order to control ClamAV.
    The manifest itself does not directly run ClamAV, but rather it contains a cron
    type to run a shell script at a random number of minutes (between 0 and 60) past
    every hour.

---
+++ modules/base/manifests/clamav.pp
@@ -0,0 +1,20 @@
+# == Class: base::clamav
+#
+# Set up virus scanning of the backup-data drive
+# After all, we don't want a virus...
+#
+class base::clamav {
+
+  file { '/usr/local/sbin/scan-backup-data.sh':
+    source => 'puppet:///modules/base/usr/local/sbin/scan-backup-data.sh',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  cron { 'clamscan-backup-data':
+    command => '/usr/local/sbin/scan-backup-data.sh',
+    minute  => fqdn_rand(60),
+    require => File['/usr/local/sbin/scan-backup-data.sh'],
+  }
+}
