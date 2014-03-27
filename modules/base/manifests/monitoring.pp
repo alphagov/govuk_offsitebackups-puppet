f731713627d153045fdb55bf01c1b337b1d9a5e8
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2014-03-27 15:42:57 +0000

    Re-work monitoring file
    Since Puppet resource can only be exported and collected by Puppet agents which
    share the same Puppet master, then a Puppet agent running in standalone,
    agentless, configuration cannot export resources. This re-work fixes this, and
    includes class parameters to look up the FQDN and hostname from Hiera.

--- modules/base/manifests/monitoring.pp
+++ modules/base/manifests/monitoring.pp
@@ -4,19 +4,31 @@
 #   - lack of disk space on the back-up drive
 #   - lack of replies to ICMP requests
 
-class base::monitoring {
+class base::monitoring (
+  [
+    $fqdn,
+    $hostname,
+  ]
+){
 
-  @@icinga::check { "check_disk_${::hostname})":
+  icinga::host { $fqdn:
+    hostalias => $fqdn,
+    address   => $fqdn,
+  }
+
+  icinga::check { "check_disk_${fqdn}":
     check_command       => 'check_nrpe_1arg!check_xvda1',
     use                 => 'govuk_high_priority',
     service_description => 'high disk usage'
+    use                 => 'govuk_high_priority',
+    host_name           => $fqdn,
   }
 
-  @@icinga::check { "check_ping_${::hostname})":
+  icinga::check { "check_ping_${fqdn}":
     check_command       => 'check_ping!100.0,20%!500.0,60%',
     notification_period => '24x7',
     use                 => 'govuk_high_priority',
     service_description => 'unable to ping',
-    host_name           => $::fqdn,
+    host_name           => $fqdn
   }
 }
