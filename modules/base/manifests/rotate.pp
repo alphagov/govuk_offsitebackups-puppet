1a00b17ed975e57c3db828bb62f7717720b68876
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2014-05-01 16:05:25 +0100

    Add `rotate` class
    This class creates a command, to be run by the `govuk-backup` user, in order
    to remove back-ups created more than 30 days ago.
    
    This works fine:
    
    vagrant@obnode0:~$ sudo crontab -u govuk-backup -l
    0 9 * * * find /dev/mapper/backup-data/* -type f -mtime +30 -delete

---
+++ modules/base/manifests/rotate.pp
@@ -0,0 +1,14 @@
+# rotate.pp
+#
+# This file defines the cron type and options in order to ensure that
+# backed-up data held on /srv/backup-data on off-site backup machines
+# is removed after thirty days.
+
+class base::rotate {
+  cron { 'rotate_old_backups':
+    command => 'find /srv/backup-data -type f -mtime +30 -delete',
+    user    => 'govuk-backup',
+    hour    => 09,
+    minute  => 00,
+  }
+}
