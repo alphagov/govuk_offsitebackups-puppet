448da67f6d1247781667697b024bf1acd4d19809
Dan Carley <dan.carley@gmail.com>
2014-05-09 10:20:05 +0100

    Unwrap logger line in box/../jenkins.sh
    Looks like a vim-typo.

--- vcloud/box/carrenza/jenkins.sh
+++ vcloud/box/carrenza/jenkins.sh
@@ -3,7 +3,6 @@ set -eu
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 
 RUBYOPT="-r ./vcloud/box/carrenza/fog_credentials" bundle exec vcloud-launch vcloud/box/carrenza/govuk_offsitebackups.yaml
-logger -p INFO -t jenkins "DEPLOYMENT: ${JOB_NAME} ${BUILD_NUMBER}
-(${BUILD_URL})"
+logger -p INFO -t jenkins "DEPLOYMENT: ${JOB_NAME} ${BUILD_NUMBER} (${BUILD_URL})"
 bundle exec vcloud-configure-edge vcloud/net/carrenza/edge.yaml
 logger -p INFO -t jenkins "Configuring ${BUILD_NUMBER}'s network settings..."
