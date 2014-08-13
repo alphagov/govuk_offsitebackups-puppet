2a09c99c8e0c43cae1c14db519cf4769052dea51
Dan Carley <dan.carley@gmail.com>
2014-08-13 10:42:22 +0100

    Rename vcloud-{configure-edge,edge-configure}
    This tool was renamed in vcloud-edge_gateway 1.0.0:
    
    - https://github.com/gds-operations/vcloud-edge_gateway/blob/v1.1.0/CHANGELOG.md#100-2014-06-04

--- vcloud/jenkins_carrenza.sh
+++ vcloud/jenkins_carrenza.sh
@@ -7,5 +7,5 @@ export VCLOUD_HOST="myvdc.carrenza.net"
 
 RUBYOPT="-r ./vcloud/fog_credentials" bundle exec vcloud-launch vcloud/box/carrenza/govuk_offsitebackups.yaml
 logger -p INFO -t jenkins "DEPLOYMENT: ${JOB_NAME} ${BUILD_NUMBER} (${BUILD_URL})"
-RUBYOPT="-r ./vcloud/fog_credentials" bundle exec vcloud-configure-edge vcloud/net/carrenza/edge.yaml
+RUBYOPT="-r ./vcloud/fog_credentials" bundle exec vcloud-edge-configure vcloud/net/carrenza/edge.yaml
 logger -p INFO -t jenkins "Configuring ${BUILD_NUMBER}'s network settings..."
