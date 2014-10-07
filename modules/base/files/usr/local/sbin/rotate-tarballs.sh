481ec4041735aef78ee2ee84cc60908a286e4b3a
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-10-07 11:47:55 +0100

    Add disk for graphite backups
    - Adds a 100GB disk for Graphite backups (20GB a time)
    - Keeps only the last 3 days of graphite backups

--- modules/base/files/usr/local/sbin/rotate-tarballs.sh
+++ modules/base/files/usr/local/sbin/rotate-tarballs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 find /srv/backup-data -not \( -path /srv/backup-data/lost+found -prune \) -type f -mtime +30 -exec rm -f {} +
+find /srv/backup-graphite -not \( -path /srv/backup-graphite/lost+found -prune \) -type f -mtime +3 -exec rm -f {} +
