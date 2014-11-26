0942571a44ed5a2f20b6c995e514d26fd0115ae1
Dan Carley <dan.carley@gmail.com>
2014-11-26 13:49:05 +0000

    [#80824874 #82210296] Remove backup rotation
    This is done for us by the sender now that all offsite backups have been
    converted to use duplicity.
    
    By still having this cronned shell script in place we risk possibly deleting
    duplicity's data underneath it and rendering a backup chain unusable.

--- modules/base/manifests/rotate.pp
+++ modules/base/manifests/rotate.pp
@@ -1,23 +1,15 @@
 # rotate.pp
 #
-# This file defines the cron type and options in order to ensure that
-# backed-up data held on /srv/backup-data on off-site backup machines
-# is removed after thirty days.
-
+# FIXME: Remove class when deployed.
+#
 class base::rotate {
 
   file { '/usr/local/sbin/rotate-tarballs.sh':
-    source  => 'puppet:///modules/base/usr/local/sbin/rotate-tarballs.sh',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
+    ensure => absent,
   }
 
   cron { 'rotate_old_backups':
-    require => File['/usr/local/sbin/rotate-tarballs.sh'],
-    command => '/usr/local/sbin/rotate-tarballs.sh',
-    user    => 'govuk-backup',
-    hour    => 09,
-    minute  => 00,
+    ensure => absent,
+    user   => 'govuk-backup',
   }
 }
